5adb22e9a12bd3d7874a42790ab0cf386eebf1c3
aijiejie <125361152@qq.com>
2017-06-29 18:22:18 +0800

    leetcode
    leetcode题目

---
+++ src/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * Created by shaohui on 2017/3/23 0023.
+ */
+
+
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+
+}
---
+++ src/Solution.java
@@ -0,0 +1,21 @@
+/*
+import java.util.Scanner;
+
+*/
+/**
+ * Created by shaohui on 2017/3/22 0022.
+ *//*
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j;
+        for(i=0;i<nums.length;i++){
+            for(j=0;j<nums.length-1;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+    }
+
+｝*/
